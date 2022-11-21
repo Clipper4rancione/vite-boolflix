4e6981c98d717d7d65975f87b8361b1e74b82df8
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-21 17:07:18 +0100

    [add] textSearch for v-model and api Call

--- src/data/store.js
+++ src/data/store.js
@@ -3,5 +3,6 @@ import { reactive } from "vue";
 export const store = reactive({
     
     apiUrlFilm: 'https://api.themoviedb.org/3/search/movie?api_key=76150228acfafd99a70e6ef719d67680',
+    textSearch: 'matrix'
 
 })
