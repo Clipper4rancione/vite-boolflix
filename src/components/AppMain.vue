8224a53a47bf73e47973f5ad7cc791042ea95760
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-21 16:42:42 +0100

    [add] list for display content

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,20 @@ export default {
 </script>
 
 <template>
-  
+  <div class="container">
+    <div class="main-wrapper">
+        <div class="ul">
+            <li>
+                film 
+                <ul>
+                    <li>A</li>
+                    <li>B</li>
+                    <li>C</li>
+                </ul>
+            </li>
+        </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
