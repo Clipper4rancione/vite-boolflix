1ed8bb88ba159ddf1ef096227dedbd46e5a030aa
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-21 17:18:33 +0100

    [add] getMovie()

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script>
 
+// Axios
+import axios from 'axios';
 // Store
-import { store } from './data/store'
+import { store } from './data/store';
 
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
@@ -10,12 +12,32 @@ export default {
   name: 'App',
   data(){
     return{
-      store
+      store,
     }
   },
   components:{
     AppHeader,
     AppMain
+  },
+  methods:{
+    getMovie(){
+      axios.get(store.apiUrlFilm, {
+        params:{
+          query: store.textSearch,
+        }
+      })
+      .then( result => {
+        //console.log(result.data.results);
+        store.filmArray = result.data.results;
+      })
+      .catch( error => {
+        store.filmArray = [];
+        console.log(error);
+      })
+    }
+  },
+  mounted(){
+    this.getMovie();
   }
 }
 </script>
