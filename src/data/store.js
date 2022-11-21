d0a8e6152fc87f726114f89f6240b5c52faa4983
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-21 17:47:21 +0100

    cleared textSearch string

--- src/data/store.js
+++ src/data/store.js
@@ -4,6 +4,6 @@ export const store = reactive({
     
     apiUrlFilm: 'https://api.themoviedb.org/3/search/movie?api_key=76150228acfafd99a70e6ef719d67680',
     filmArray: [],
-    textSearch: 'matrix',
+    textSearch: '',
 
 })
