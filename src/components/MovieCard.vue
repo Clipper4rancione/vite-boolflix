c82d58f6fdaf81ecb507f5829d46449b0aa5d983
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-23 12:49:37 +0100

    [add] new component MovieCard

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'MovieCard',
+    props:{
+        card: Object,
+    }
+}
+</script>
+
+<template>
+        <ul>
+            <li>
+                {{card.title || card.name}} 
+            </li>
+        </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
