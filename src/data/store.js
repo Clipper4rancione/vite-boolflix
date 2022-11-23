96f000c25b6f4fc2e3a721cca65c4a29a8fc2a87
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-23 11:45:04 +0100

    cleared query string mil-2 ok

--- src/data/store.js
+++ src/data/store.js
@@ -3,9 +3,9 @@ import { reactive } from "vue";
 export const store = reactive({
     
     apiUrl: 'https://api.themoviedb.org/3/search/',
-    apiParamas:{
+    apiParams:{
         api_key: '76150228acfafd99a70e6ef719d67680',
-        query: 'matrix',
+        query: '',
         language: 'it-IT'
     },
     movie: [],
